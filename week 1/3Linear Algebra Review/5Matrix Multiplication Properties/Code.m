b248e67790e46d2a124c114f042ca8f69479662c
Lonch <517746547@qq.com>
2018-03-12 08:49:49 +0800

    Add files via upload

---
+++ week 1/3Linear Algebra Review/5Matrix Multiplication Properties/Code.m
@@ -0,0 +1,24 @@
+When multiplying the identity matrix after some matrix (A?I), the square identity matrix's dimension should match the other matrix's columns. When multiplying the identity matrix before some other matrix (I?A), the square identity matrix's dimension should match the other matrix's rows.
+
+% Initialize random matrices A and B 
+A = [1,2;4,5]
+B = [1,1;0,2]
+
+% Initialize a 2 by 2 identity matrix
+I = eye(2)
+
+% The above notation is the same as I = [1,0;0,1]
+
+% What happens when we multiply I*A ? 
+IA = I*A 
+
+% How about A*I ? 
+AI = A*I 
+
+% Compute A*B 
+AB = A*B 
+
+% Is it equal to B*A? 
+BA = B*A 
+
+% Note that IA = AI but AB != BA
