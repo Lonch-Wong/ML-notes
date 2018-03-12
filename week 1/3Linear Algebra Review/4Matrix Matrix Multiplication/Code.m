673d9ca7ae423efb90aad6aededab3b3215d1c2f
Lonch <517746547@qq.com>
2018-03-12 08:50:30 +0800

    Add files via upload

---
+++ week 1/3Linear Algebra Review/4Matrix Matrix Multiplication/Code.m
@@ -0,0 +1,14 @@
+To multiply two matrices, the number of columns of the first matrix must equal the number of rows of the second matrix.
+
+For example:
+
+% Initialize a 3 by 2 matrix 
+A = [1, 2; 3, 4;5, 6]
+
+% Initialize a 2 by 1 matrix 
+B = [1; 2] 
+
+% We expect a resulting matrix of (3 by 2)*(2 by 1) = (3 by 1) 
+mult_AB = A*B
+
+% Make sure you understand why we got that result
