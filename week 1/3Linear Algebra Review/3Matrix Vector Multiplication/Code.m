02fa0751ebcf6061095f29bceb54280ef33b436e
Lonch <517746547@qq.com>
2018-03-12 08:48:51 +0800

    Add files via upload

---
+++ week 1/3Linear Algebra Review/3Matrix Vector Multiplication/Code.m
@@ -0,0 +1,12 @@
+Below is an example of a matrix-vector multiplication. Make sure you understand how the multiplication works. Feel free to try different matrix-vector multiplications.
+
+
+% Initialize matrix A 
+A = [1, 2, 3; 4, 5, 6;7, 8, 9] 
+
+% Initialize vector v 
+v = [1; 1; 1] 
+
+% Multiply A * v
+Av = A * v
+
