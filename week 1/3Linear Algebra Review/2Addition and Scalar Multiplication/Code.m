91252c87fa33101ffba7fa3b886a359d2de190c4
Lonch <517746547@qq.com>
2018-03-12 08:48:19 +0800

    Add files via upload

---
+++ week 1/3Linear Algebra Review/2Addition and Scalar Multiplication/Code.m
@@ -0,0 +1,25 @@
+Experiment below with the Octave/Matlab commands for matrix addition and scalar multiplication. Feel free to try out different commands. Try to write out your answers for each command before running the cell below.
+
+% Initialize matrix A and B 
+A = [1, 2, 4; 5, 3, 2]
+B = [1, 3, 4; 1, 1, 1]
+
+% Initialize constant s 
+s = 2
+
+% See how element-wise addition works
+add_AB = A + B 
+
+% See how element-wise subtraction works
+sub_AB = A - B
+
+% See how scalar multiplication works
+mult_As = A * s
+
+% Divide A by s
+div_As = A / s
+
+% What happens if we have a Matrix + scalar?
+add_As = A + s
+
+
